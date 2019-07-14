e603da9ffcd109ecfc51122957c07952e8754eff
myth-linux <zdhgsm@163.com>
2019-07-14 14:13:54 +0800

    原始版本

---
+++ client.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(char* message);
+
+int main(int argc,char*argv[])
+{
+    int sock;
+    struct sockaddr_in serv_addr;
+    char message[30];
+    int idx = 0,read_len = 0;
+    int str_len = 0;
+
+    if(argc != 3)
+    {
+        printf("Usage: %s <IP> <prot>\n",argv[0]);
+        exit(1);
+    }
+
+    sock = socket(PF_INET,SOCK_STREAM,0);
+    if(-1 == sock)
+    {
+        error_handling("socket() error");
+    }
+
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if(-1 == connect(sock,(struct sockaddr*)& serv_addr,sizeof(serv_addr)))
+    {
+        error_handling("connect() error");
+    }
+
+    while (read_len = read(sock,&message[idx++],1))
+    {
+        if(-1 == read_len)
+    {
+        error_handling("read() error");
+    }
+        str_len +=read_len;
+    }
+    
+    
+    printf("%s\n",message);
+    printf("%d\n",str_len);
+    close(sock);
+
+    return 0;
+}
+
+void error_handling(char* message)
+{
+    fputs(message,stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
