7915af4da3e9a48d01ff365188b76a3f30f7a427
myth-linux <zdhgsm@163.com>
2019-07-15 16:51:19 +0800

    迭代服务器为多进程并发服务器

--- server.c
+++ server.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define MAX_SIZE 1024
+#define BUF_SIZE 1024
 
 void error_handling(char* message);
+void read_childproc(int sig);
 
 int main(int argc,char* argv[])
 {
@@ -19,14 +22,18 @@ int main(int argc,char* argv[])
     struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_size;
 
-    char message[MAX_SIZE]; //= "Hello World!Hello World!Hello World!";
-    int str_len =0;
+    //struct sigaction act;
+    char message[BUF_SIZE]; 
+    int str_len = 0,state = 0;
+    pid_t pid;
 
     if(argc != 2)
     {
         printf("Usage:%s <port>\n",argv[0]);
         exit(1);
     }
+
+    
 
     serv_sock = socket(PF_INET,SOCK_STREAM,0);
     if(-1 == serv_sock)
@@ -49,35 +56,52 @@ int main(int argc,char* argv[])
     {
         error_handling("listen() error");
     }
-
-    clnt_addr_size = sizeof(clnt_addr);
     
     
-    for(int i =0;i<5;++i)
+    while(1)
     {
+
+
+        clnt_addr_size = sizeof(clnt_addr);
         clnt_sock = accept(serv_sock,(struct sockaddr*)&clnt_addr,&clnt_addr_size);
         if (-1 == clnt_sock)
         {
-            error_handling("accept() errpr");
+            //error_handling("accept() errpr");
+            continue;
         }
         else
         {
-            printf("IP: %s\n",inet_ntoa(clnt_addr.sin_addr));
-            printf("Connected client %d\n",i+1);
+            //printf("IP: %s\n",inet_ntoa(clnt_addr.sin_addr));
+            printf("New  client Connected... IP: %s\n",inet_ntoa(clnt_addr.sin_addr));
             
         }
-        while (1)
+        pid = fork();
+        if(-1 == pid)
         {
-            str_len = read(clnt_sock,message,MAX_SIZE);
-            if (str_len == 0)
-            {
-                error_handling("reda()=0");
-                break;
-            }
-            printf("client message: %s\n",message);
-            write(clnt_sock,message,str_len);
+            close(clnt_sock);
+            continue;
         }
-        close(clnt_sock);
+        else if(0 == pid)
+        {
+            close(serv_sock);
+            while (str_len = read(clnt_sock,message,BUF_SIZE)!=0)
+            {
+                
+                // str_len = read(clnt_sock,message,BUF_SIZE);
+                // if (str_len == 0)
+                // {
+                //     error_handling("reda()=0");
+                //     break;
+                // }
+                //printf("client message: %s\n",message);
+                write(clnt_sock,message,str_len);
+            }
+                close(clnt_sock);
+                puts("client disconnected......");
+                return 0;
+        }
+        else
+            close(clnt_sock);
     }
     close(serv_sock);
     return 0;
@@ -88,4 +112,12 @@ void error_handling(char* message)
     fputs(message,stderr);
     fputc('\n',stderr);
     exit(1);
+}
+
+void read_childproc(int sig)
+{
+    pid_t pid;
+    int status;
+    pid = waitpid(-1,&status,WNOHANG);
+    printf("removed proc id: %d\n",pid);
 }
